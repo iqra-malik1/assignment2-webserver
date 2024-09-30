25b57dad704d877fdee11d4d1345df8bc8c6db96
iqra-malik1 <im2785@nyu.edu>
2024-09-29 23:25:24 -0400

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -38,7 +38,7 @@ def webServer(port=13331):
       #Send the content of the requested file to the client (don't forget the headers you created)!
       #Send everything as one send command, do not send one line/item at a time!
       
-      connectionSocket.send(validResponseHeader, outputdata).encode()
+      connectionSocket.sendall(validResponseHeader, outputdata).encode()
 
       connectionSocket.close() #closing the connection socket
       
