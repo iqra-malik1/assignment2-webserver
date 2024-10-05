283239b390c92eff883a1735a97718d015c75c49
iqra-malik1 <im2785@nyu.edu>
2024-10-04 22:42:13 -0400

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -16,26 +16,26 @@ def webServer(port=13331):
     try:
        #a client is sending you a message 
       message = connectionSocket.recv(1024).decode()
-      filename = message.split()[1]
+      fileName = message.split()[1]
       
       #opens the client requested file. 
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:], 'rb')
+      f = open(fileName[1:], 'rb')
 
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
 
       validResponse = b"HTTP/1.1 200 OK\r\n"
-      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
-      outputdata += b"Server: SimplePythonServer\r\n"
-      outputdata += b"Connection: Close \r\n\r\n"
+      outputResponse = b"Content-Type: text/html; charset=UTF-8\r\n"
+      outputResponse += b"Server: SimplePythonServer\r\n"
+      outputResponse += b"Connection: Close \r\n\r\n"
                
       for i in f: #for line in file
-        outputdata += f.read()
+        outputResponse += f.read()
         
       #Send the content of the requested file to the client (don't forget the headers you created)!
       #Send everything as one send command, do not send one line/item at a time!
       
-      connectionSocket.sendall(validResponse + outputdata + f.read())
+      connectionSocket.sendall(validResponse + outputResponse + f.read())
 
       connectionSocket.close() #closing the connection socket
       
@@ -47,9 +47,9 @@ def webServer(port=13331):
       errorResponse += b"Content-Type: text/html; charset=UTF-8\r\n"
       errorResponse += b"Server: SimplePythonServer\r\n"
       errorResponse += b"Connection: Close \r\n\r\n"
-      errorResponseBody = b"<html><body><h1>404 Not Found</h1></body></html>\r\n"
+      errorResponseHTML = b"<html><body><h1>404 Not Found</h1></body></html>\r\n"
       
-      connectionSocket.sendall(errorResponse + errorResponseBody)
+      connectionSocket.sendall(errorResponse + errorResponseHTML)
 
       #Close client socket
       connectionSocket.close()
